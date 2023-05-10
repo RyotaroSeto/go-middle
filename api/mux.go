db93d9c34522b6b857290db140dd439f5597effe
RyotaroSeto <hahahahawaiwai321@gmail.com>
2023-05-10 20:59:43 +0900

    service定義

--- api/mux.go
+++ api/mux.go
@@ -9,18 +9,18 @@ import (
 	"github.com/gorilla/mux"
 )
 
-func NewMux() {
+func NewMux(con *handlers.MyAppController) {
 
 	r := mux.NewRouter()
 
-	r.HandleFunc("/hello", handlers.HelloHandler).Methods(http.MethodGet)
+	r.HandleFunc("/hello", con.HelloHandler).Methods(http.MethodGet)
 
-	r.HandleFunc("/article", handlers.PostArticleHandler).Methods(http.MethodPost)
-	r.HandleFunc("/article/list", handlers.ArticleListHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/{id:[0-9]+}", handlers.ArticleDetailHandler).Methods(http.MethodGet)
-	r.HandleFunc("/article/nice", handlers.PostNiceHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article", con.PostArticleHandler).Methods(http.MethodPost)
+	r.HandleFunc("/article/list", con.ArticleListHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/{id:[0-9]+}", con.ArticleDetailHandler).Methods(http.MethodGet)
+	r.HandleFunc("/article/nice", con.PostNiceHandler).Methods(http.MethodPost)
 
-	r.HandleFunc("/comment", handlers.PostCommentHandler).Methods(http.MethodPost)
+	r.HandleFunc("/comment", con.PostCommentHandler).Methods(http.MethodPost)
 
 	log.Println("server start at port 8000")
 	log.Fatal(http.ListenAndServe(":8000", r))
--- main.go
+++ main.go
@@ -1,11 +1,41 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
+	"log"
 	"sample/api"
+	"sample/handlers"
+	"sample/services"
 
 	_ "github.com/go-sql-driver/mysql"
 )
 
+var (
+	dbUser     = "docker"
+	dbPassword = "docker"
+	dbDatabase = "docker"
+	dbHost     = "mysql"
+	dbPort     = "3306"
+	dbConn     = fmt.Sprintf("%s:%s@tcp(%s:%s)/%s?parseTime=true", dbUser, dbPassword, dbHost, dbPort, dbDatabase)
+)
+
 func main() {
-	api.NewMux()
+	db, err := connectDB()
+	if err != nil {
+		log.Println("fail to connect DB")
+		return
+	}
+
+	ser := services.NewMyAppService(db)
+	con := handlers.NewMyAppController(ser)
+	api.NewMux(con)
+}
+
+func connectDB() (*sql.DB, error) {
+	db, err := sql.Open("mysql", dbConn)
+	if err != nil {
+		return nil, err
+	}
+	return db, nil
 }
